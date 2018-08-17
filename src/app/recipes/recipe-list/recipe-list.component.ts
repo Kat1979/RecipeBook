db1f77457744270ebf61ba6b03f86278c1b7a123
TESTPC <floyd_cjr@hotmail.com>
2018-08-17 16:18:28 -0400

    Commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,15 +8,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://media.defense.gov/2016/Sep/20/2001637436/1088/820/0/160913-F-BO262-1020.JPG'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://media.defense.gov/2016/Sep/20/2001637436/1088/820/0/160913-F-BO262-1020.JPG'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://media.defense.gov/2016/Sep/20/2001637436/1088/820/0/160913-F-BO262-1020.JPG'),
+    new Recipe('A another Test Recipe', 'This is simply a test', 'https://media.defense.gov/2016/Sep/20/2001637436/1088/820/0/160913-F-BO262-1020.JPG'),
+    new Recipe('A third Test Recipe', 'This is simply a test', 'https://media.defense.gov/2016/Sep/20/2001637436/1088/820/0/160913-F-BO262-1020.JPG'),
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+  onRecipeSelected(recipe: Recipe){
+   this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/shoping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shoping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef,EventEmitter } from '@angular/core';
+import { Ingredient } from '../../Shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountIput') amountInputRef: ElementRef;
+
+  ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddItem(){
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingamount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingamount)
+    this.ingredientAdded.emit();
+  }
 }
