e9521c547fb58052dca72eaf166b85f6757b4b4c
TESTPC <floyd_cjr@hotmail.com>
2018-08-24 08:20:38 -0400

    Shoppinglist

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,11 @@
-export class ShoppingLiistService{
-    
+import { Ingredient } from '../Shared/ingredient.model';
+
+export class ShoppingListService{
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+      ];
+      getIngredents(){
+          return this.ingredients.slice();
+      }
 }
